c2b6ec41e87fe3b51f1e90d1a6ff86224db92506
J-DI <jamesdisciullo14@gmail.com>
2025-10-05 20:23:43 -0700

    Added SCurve Trajectory parameter function + Helpers

--- lib/SCurveTrajectory.h
+++ lib/SCurveTrajectory.h
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846f
+#endif
 
 //These are for the elbow motor, and will need to make a helper function to extract for rover depending on esc
-#define MOTOR_TORQUE  405e-3  // Nm
-#define MOTOR_MOMENT 13.31e-6 // kg·m²
-//
+#define MOTOR_TORQUE  405e-3f  // Nm
+#define MOTOR_MOMENT 13.31e-6f // kg·m²
+#define SAMPLING_TIME 0.001 // in s, 1kHz
+#define DEBUG_MODE 1 // 1 for debug, zero for actual motor application
+#define VEL_FILTER_COEFFICIENT 0.2 // Alter this for smoothening out filter
+#define TRAJ_MAX_POINTS 8000 // NMaximum size of array calculated based on worst case Delta_T/Ts
+
+
+//Externs
+extern float s_traj_theta[TRAJ_MAX_POINTS]; // Array to hold setpoints
+extern int s_traj_len;                 // Length of setpoint array <--make this uin16_t in stm
 
 typedef struct
 {
@@ -32,13 +45,31 @@ typedef struct
   float A_MAX; // M/s^2 - Max angular acceleration
   float J_MAX; // M/s^3 - Max rate of change of angular acceleration
 
+  bool isExecutingTrajectory; // Is the current ESC current executing a movement
+
 } PosCtrlHandle;
 
+/*
+This struct is to be used as a velocity filter that holds the acual values of the motor state, retrieved and updated through the encoder itself
+*/
+typedef struct {
+    float theta_prev;     // last angle reading [rad]
+    float omega;          // filtered velocity [rad/s]
+    float omega_prev;
+    float accel;
+    float alpha_coeff;    // filter coefficient (0..1)
+    float Ts;             // sample period [s]
+} VelocityFilter;
 
 
 //Relevent Function Prototypes
+void STrajectoryInit(PosCtrlHandle *pHandle);
 
 
 //Helpers
 float getAMax(float motorTorque, float motorMomentIntertia);
 float getJMax(float AMax, float timeToAMax) ;
+float getEstimatedTrajectoryTime (float currentAngle, float targetAngle, float angularVelocity);
+float computeTrajectoryParameters(PosCtrlHandle *pHandle, float startingAngle, float movementDuration, float totalAngleMovement);
+float degreesToRad(float positionDegrees);
+float radToDegrees(float positionRad);
--- src/SCurveTrajectory.c
+++ src/SCurveTrajectory.c
@@ -2,16 +2,28 @@
 #include <stdlib.h>
 #include <math.h>
 #include "SCurveTrajectory.h"
+// #include "can_processing.h"
 
 
+/*
+Returns the maximum angular acceleration based on physical motor parameters
+*/
 float getAMax(float motorTorque, float motorMomentIntertia) {
     return motorTorque / motorMomentIntertia;
 }
 
+/*
+Returns the maximum Jerk the arm can output. NOTE: This value is experimentally determined, and is essentially just an indicator
+of how long it should take to hit the target angular acceleration to smooth it out instead of it being instantaneous (ms)
+*/
 float getJMax(float AMax, float timeToAMax) {
-    return AMax / timeToAMax;
+    return AMax / (timeToAMax / 1000); // time in ms
 }
 
+
+/*
+This function is used to initialize all of the struct parameters initially in the code
+*/
 void STrajectoryInit(PosCtrlHandle *pHandle){
 
   pHandle->MovementDuration = 0.0f;
@@ -23,6 +35,7 @@ void STrajectoryInit(PosCtrlHandle *pHandle){
   pHandle->SubStep[4] = 0.0f;
   pHandle->SubStep[5] = 0.0f;
 
+  pHandle->SamplingTime = SAMPLING_TIME;
   pHandle->SubStepDuration = 0;
 
   pHandle->Jerk = 0.0f;
@@ -34,18 +47,102 @@ void STrajectoryInit(PosCtrlHandle *pHandle){
   pHandle->ThetaPrev = 0.0f;
 
 
-  phandle->A_MAX = getAMax(MOTOR_TORQUE, MOTOR_MOMENT); // M/s^2 - Max angular acceleration
-  phandle-> J_MAX; // M/s^3 - Max rate of change of angular acceleration
+  pHandle->A_MAX = getAMax(MOTOR_TORQUE, MOTOR_MOMENT); // M/s^2 - Max angular acceleration
+  pHandle-> J_MAX = getJMax(pHandle->A_MAX, 10);
 }
 
+void velocityFilterInit(VelocityFilter *pHandle){
+    pHandle ->theta_prev = 0;
+    pHandle ->omega = 0;
+    pHandle ->alpha = 0;
+    pHandle ->Ts = SAMPLING_TIME;
+}
+
+
+/*
+This function calculates the required parameters and saves them to the trajectory struct, which 
+are used by the algorythm to produce the S curve
+  * @param  pHandle handler of the current instance of the Position Control component.
+  * @param  startingAngle Current mechanical position.
+  * @param  angleStep Target mechanical position.
+  * @param  movementDuration Duration to reach the final position (in seconds).
+
+*/
+float computeTrajectoryParameters(PosCtrlHandle *pHandle, float startingAngle, float movementDuration, float totalAngleMovement){
+
+    float fMinimumStepDuration;
+    fMinimumStepDuration = (9.0f * pHandle->SamplingTime);
+
+    /* WARNING: Movement duration value is rounded to the nearest valid value
+       [(DeltaT/9) / SamplingTime]:  shall be an integer value */
+    pHandle->MovementDuration = (float)((int)(movementDuration / fMinimumStepDuration)) * fMinimumStepDuration;
+
+    pHandle->StartingAngle = startingAngle;
+    pHandle->AngleStep = totalAngleMovement;
+    pHandle->FinalAngle = startingAngle + totalAngleMovement;
+
+    /* SubStep duration = DeltaT/9  (DeltaT represents the total duration of the programmed movement) */
+    pHandle->SubStepDuration = pHandle->MovementDuration / 9.0f;
+
+    /* Sub step of acceleration phase */
+    pHandle->SubStep[0] = 1 * pHandle->SubStepDuration;   /* Sub-step 1 of acceleration phase */
+    pHandle->SubStep[1] = 2 * pHandle->SubStepDuration;   /* Sub-step 2 of acceleration phase */
+    pHandle->SubStep[2] = 3 * pHandle->SubStepDuration;   /* Sub-step 3 of acceleration phase */
+
+    /* Sub step of  deceleration Phase */
+    pHandle->SubStep[3] = 6 * pHandle->SubStepDuration;   /* Sub-step 1 of deceleration phase */
+    pHandle->SubStep[4] = 7 * pHandle->SubStepDuration;   /* Sub-step 2 of deceleration phase */
+    pHandle->SubStep[5] = 8 * pHandle->SubStepDuration;   /* Sub-step 3 of deceleration phase */
+
+    /* Jerk (J) to be used by the trajectory calculator to integrate (step by step) the target position.
+       J = DeltaTheta/(12 * A * A * A)  => DeltaTheta = final position and A = Sub-Step duration */
+    pHandle->Jerk = pHandle->AngleStep / (12 * pHandle->SubStepDuration * pHandle->SubStepDuration * pHandle->SubStepDuration);
+
+    /* Speed cruiser = 2*J*A*A) */
+    pHandle->CruiseSpeed = 2 * pHandle->Jerk * pHandle->SubStepDuration * pHandle->SubStepDuration;
+    pHandle->ElapseTime = 0.0f;
+
+}
+
+/*
+This function is called after each time a new setpoint is provided to the esc, so a 1Khz. It is used to update the velocity
+filter object, so that we may extract the best possible state of the current motor being controlled
+*/
+void updateVelocityFilter(VelocityFilter pHandle){
+  //This is the version that will work in our escs, but not in testing
+  float currentPosition = getCurrentPosition();
+  pHandle->omega = currentPosition - pHandle->pre
+
+}
 
 
 int main (){
 
   // Put this stuff in the init section
-
   PosCtrlHandle SCurveTrajectory;
   STrajectoryInit(&SCurveTrajectory);
-  
+
+}
+
+
+/*
+This function computes the estimated time it will take to execute the desired motion
+
+Inputs: Current joint angle   : rad
+        Target Position angel : rad
+        desired angular velocity  : rad/sec
+Outputs: estimated trajectory time : ms
+*/
+float getEstimatedTrajectoryTime (float currentAngle, float targetAngle, float angularVelocity){
+  return (fabs(currentAngle - targetAngle) / angularVelocity) * 1000 ; // Want ms here
+}
+
+// Local helpter functions -- include the other .c custum fole for these 
+float degreesToRad(float positionDegrees){
+	return positionDegrees*(M_PI /180.0f);
+}
+
+float radToDegrees(float positionRad){
+	return (positionRad*180/M_PI);
 
 }
